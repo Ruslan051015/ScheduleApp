943a65b3db0547b738ff164fb474c8940bab8e18
ruslan0515015 <ruslaniosdev@gmail.com>
2024-11-02 19:23:09 +0300

    Initial Commit

---
+++ Schedule App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Schedule App
+//
+//  Created by Руслан Халилулин on 02.11.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Schedule App/Schedule_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Schedule_AppApp.swift
+//  Schedule App
+//
+//  Created by Руслан Халилулин on 02.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct Schedule_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
